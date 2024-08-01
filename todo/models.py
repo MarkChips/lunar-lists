b65442697a16ddae52f9e98bea4d8aebf05ff723
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-08-01 11:19:36 +0000

    feat: :sparkles: add List and Task model class, link up admin.py, and makemigrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class List(models.Model):
+    list_name = models.CharField(max_length=200, unique=False)
+    due_by = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    user_ID = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="lists"
+    )
+
+
+class Task(models.Model):
+    task_description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    list_ID = models.ForeignKey(
+        List, on_delete=models.CASCADE, related_name="tasks"
+    )
