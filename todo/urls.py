30dec3a4ae57dd6824ffb22a49484694fc48f758
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-08-05 13:27:05 +0000

    feat: :sparkles: add list view and redirect to user's list view after login

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
-from . import views
+from django.contrib.auth.decorators import login_required
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("", views.HomePage.as_view(), name='home'),
+    path('lists/', login_required(views.list_view), name='list_view'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from .models import List
 
 # Create your views here.
 class HomePage(TemplateView):
@@ -7,3 +9,9 @@ class HomePage(TemplateView):
     Displays home page
     """
     template_name = 'todo/index.html'
+
+@login_required
+def list_view(request):
+    user_lists = List.objects.filter(user_ID=request.user)
+
+    return render(request, 'todo/todo_list.html', {'lists': user_lists})
