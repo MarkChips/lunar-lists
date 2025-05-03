1a3506b6922eb627839412aa56ced4a70e0b627e
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-05-03 15:12:06 +0100

    test: ✅ update TestListForm to test missing fields

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -8,6 +8,12 @@ class TestListForm(TestCase):
         list_form = ListForm({'list_name': 'New List', 'due_by': '1995-10-03'})
         self.assertTrue(list_form.is_valid(), msg='Form is not valid')
 
-    def test_form_is_invalid(self):
-        list_form = ListForm({'list_name': '', 'due_by': ''})
-        self.assertFalse(list_form.is_valid(), msg='Form is valid')
+    def test_list_name_is_required(self):
+        list_form = ListForm({'list_name': '', 'due_by': '1995-10-03'})
+        self.assertFalse(list_form.is_valid(
+        ), msg='List name was not provided, but the form is valid')
+
+    def test_due_by_is_required(self):
+        list_form = ListForm({'list_name': 'New List', 'due_by': ''})
+        self.assertFalse(list_form.is_valid(),
+                         msg='Due by date was not provided, but the form is valid')
