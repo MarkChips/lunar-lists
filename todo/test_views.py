a4d763bf45a53aeb5b233bd825ebcfed8208f468
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-05-05 18:21:41 +0100

    feat: ✨ add login decorator for test methods

--- todo/test_views.py
+++ todo/test_views.py
@@ -28,22 +28,25 @@ class TestTodoViews(TestCase):
         )
         self.task.save()
 
+    def login(func):
+        def wrapper(self):
+            self.client.login(
+                username='myUsername',
+                password='myPassword'
+            )
+            func(self)
+        return wrapper
+
+    @login
     def test_render_list_view_with_list_form(self):
-        self.client.login(
-            username='myUsername',
-            password='myPassword'
-        )
         response = self.client.get(reverse('list_view'))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'List name', response.content)
         self.assertIn(b'Oct. 3, 1995', response.content)
         self.assertIsInstance(response.context['list_form'], ListForm)
 
+    @login
     def test_successful_list_creation(self):
-        self.client.login(
-            username='myUsername',
-            password='myPassword'
-        )
         list_data = {
             'list_name': 'New list',
             'due_by': '2025-05-10'
@@ -58,11 +61,8 @@ class TestTodoViews(TestCase):
         # Check for the success message
         self.assertIn(b'New lunar list created!', response.content)
 
+    @login
     def test_list_delete(self):
-        self.client.login(
-            username='myUsername',
-            password='myPassword'
-        )
         response = self.client.post(
             reverse('list_delete', args=[self.list.id]), follow=True)
         self.assertRedirects(response, reverse('list_view'))
